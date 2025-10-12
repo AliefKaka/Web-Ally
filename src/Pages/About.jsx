7a90c247d225d512a9506e392a2df2e877db1b51
AliefKaka <aliefkaka123@gmail.com>
2025-10-12 16:12:34 +0700

    Update About.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -240,7 +240,7 @@ const AboutPage = () => {
       </div>
 
             <div className="flex flex-col lg:flex-row items-center lg:items-start gap-4 lg:gap-4 lg:px-0 w-full">
-              <a href="https://qu.ax/dsmBF.jpg" className="w-full lg:w-auto">
+              <a href="https://s6.imgcdn.dev/YsiHw9.png" className="w-full lg:w-auto">
               <button 
                 data-aos="fade-up"
                 data-aos-duration="800"
